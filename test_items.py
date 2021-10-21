6660e1eff8d72db2e30b0ca2f2fe6702b25f9fa9
Jon Jon <jon@exenple.com>
2021-10-21 15:31:15 +0300

    add assert

--- test_items.py
+++ test_items.py
@@ -1,10 +1,8 @@
 import time
-from selenium import webdriver
-link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
-
 
 def test_button_add_to_basket(browser):
-
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(10)
-    #browser.fimd_element_by_xpath('//button [@value="Добавить в корзину"]')
+    time.sleep(5)
+
+    assert browser.find_elements_by_xpath('//button [@class="btn btn-lg btn-primary btn-add-to-basket"]'), "Кнопки нет на экране"
