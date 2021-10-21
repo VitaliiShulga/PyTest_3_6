3070a179b141b7ddc4c8186434c60a7909fb0e3e
Jon Jon <jon@exenple.com>
2021-10-21 16:15:20 +0300

    new assert

--- test_items.py
+++ test_items.py
@@ -1,8 +1,8 @@
 import time
 
-def test_button_add_to_basket(browser):
+def test_button_add_to_basket_on_screan(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(30)
-    button = browser.find_elements_by_css_selector('.bt-add-to-basket')
+    time.sleep(5)
+    button = browser.find_elements_by_css_selector('.btn-add-to-basket')
     assert button, 'button "add to basket" not found on page'
