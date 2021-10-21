2ede1bf3bf5c817c92f9b64a7b8ae8c19c5d724c
Jon Jon <jon@exenple.com>
2021-10-21 11:07:48 +0300

    create confest.py

---
+++ confest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose the any language: ")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    lang = request.config.getoption("language")
+    options = Options
+    #print("\nstart browser for test..")
+    browser = webdriver.Chrome(options=lang)
+    yield browser
+    #print("\nquit browser..")
+    browser.quit()
