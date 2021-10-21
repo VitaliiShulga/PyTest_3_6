5caa40ddc3553533c72b9d24ad1f5c3293ce1445
Jon Jon <jon@exenple.com>
2021-10-21 15:34:52 +0300

    add assert

--- test_items.py
+++ test_items.py
@@ -3,6 +3,6 @@ import time
 def test_button_add_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(5)
-
-    assert browser.find_elements_by_xpath('//button [@class="btn btn-lg btn-primary btn-add-to-basket"]'), "Кнопки нет на экране"
+    time.sleep(30)
+    button = browser.find_elements_by_xpath('//button [@class="btn btn-lg btn-primary btn-add-to-basket"]')
+    assert button, "Кнопки нет на экране"
