3cff4c6de2dd6edeb9c5002f2ac63a05a35f3e0a
Jon Jon <jon@exenple.com>
2021-10-21 15:51:59 +0300

    new assert

--- test_items.py
+++ test_items.py
@@ -4,5 +4,5 @@ def test_button_add_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     time.sleep(30)
-    button = browser.find_elements_by_xpath('//button [@class="btn btn-lg btn-primary btn-add-to-basket"]')
-    assert button, "Кнопки нет на экране"
+    button = browser.find_elements_by_css_selector('.bt-add-to-basket')
+    assert button, 'button "add to basket" not found on page'
